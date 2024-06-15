c346d09f4a1e28fdd7153f1cda96eeb4b7d01e49
dakotairsik <dakotairsik@gmail.com>
2024-06-15 10:14:43 -0400

    Fifth monster wave. Fixing some prefab components that didn't save.

---
+++ Assets/Scripts/Mechanics/Controllers/FlyingMajor.cs
@@ -0,0 +1,8 @@
+﻿namespace NeonLadder.Mechanics.Controllers
+{
+    public class FlyingMajor : Major
+    {
+        protected override float AttackRange { get; set; } = 5f;
+        protected override bool RetreatWhenTooClose { get; set; } = true;
+    }
+}
